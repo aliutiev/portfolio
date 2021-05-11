26a333c872889ab4d39103d4a93bf83d63816e3c
Andrew L <45078400+Spartinii@users.noreply.github.com>
2021-05-11 17:32:48 -0400

    Add files via upload

---
+++ sql/a1/q1.py
@@ -0,0 +1,15 @@
+"""-------------------------------------------------------Provides Python mysql.connector connection to a database.-------------------------------------------------------Author:  Andrew LiutievID:      180635320Email:   liut5320@wlu.ca__updated__ = "2020-02-05"-------------------------------------------------------"""
+from asgn01 import keyword_table
+from Connect import Connect
+the_tunnel = Connect("dcris.txt")
+rs = keyword_table(the_tunnel) 
+
+for r in rs:
+    print(r)
+the_tunnel = Connect("dcris.txt")
+rows = keyword_table(the_tunnel,keyword_id= 35) print("----------------------------------------")
+for r in rs:
+    print(r)
+
+
+
---
+++ sql/a1/q2.py
@@ -0,0 +1,29 @@
+"""-------------------------------------------------------Provides Python mysql.connector connection to a database.-------------------------------------------------------Author:  Andrew LiutievID:      180635320Email:   liut5320@wlu.ca__updated__ = "2020-02-05"-------------------------------------------------------"""
+from asgn01 import pub_table
+from Connect import Connect
+the_connector_tunnel = Connect("dcris.txt")
+rs = pub_table(the_connector_tunnel) 
+for r in rs:
+    print(r)
+    
+the_connector_tunnel = Connect("dcris.txt")
+rs = pub_table(the_connector_tunnel,member_id= 33) 
+print("----------------------------------------Member_id")
+for r in rs:
+    print(r)
+
+
+
+the_connector_tunnel = Connect("dcris.txt")
+rs = pub_table(the_connector_tunnel,pub_type_id = "b") 
+print("----------------------------------------Pub_type")
+for r in rs:
+    print(r)
+ 
+
+the_connector_tunnel = Connect("dcris.txt")
+rs = pub_table(the_connector_tunnel,member_id= 33, pub_type_id= "b" ) 
+print("----------------------------------------MEMBER & PUB")
+for r in rs:
+    print(r)
+    
