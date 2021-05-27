949cf82768e7a799cba279d6079962767f49f841
Andrew L <45078400+Spartinii@users.noreply.github.com>
2021-05-27 15:49:12 -0400

    Delete c directory

--- c/README.md
+++
@@ -1,1 +0,0 @@
-Sample assignments using C programming to complete basic tasks & basic functions using various data structures (2019)
--- c/a3/mystring.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int letter_count(char *);
-void lower_case(char *);
-int word_count(char *);
-void trim(char *);
