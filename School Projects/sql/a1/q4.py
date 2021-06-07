d5359263aeb1c28adcb25e7e2f95064415f2faee
Andrew L <45078400+Spartinii@users.noreply.github.com>
2021-06-07 12:32:39 -0400

    Update q4.py

--- School Projects/sql/a1/q4.py
+++ School Projects/sql/a1/q4.py
@@ -1,30 +1,70 @@
-"""-------------------------------------------------------Provides Python mysql.connector connection to a database.-------------------------------------------------------Author:  Andrew LiutievID:      180635320Email:   liut5320@wlu.ca__updated__ = "2020-02-05"-------------------------------------------------------"""from Connect import Connect
-from asgn01 import expertise
-
-the_connector_tunnel = Connect("dcris.txt")
-cols = expertise(the_connector_tunnel) 
-
+"""
+-------------------------------------------------------
+Provides Python mysql.connector connection to a database.
+-------------------------------------------------------
+Author:  Andrew Liutiev
+ID:      180635320
+Email:   liut5320@wlu.ca
+__updated__ = "2020-02-05"
+-------------------------------------------------------
+"""
+from Connect import Connect
+from asgn01 import expertise
+
+
+
+tunnel = Connect("dcris.txt")
+cols = expertise(tunnel) 
+
+
+
+
+
+
 for col in cols:
-    print(col)
-    
-the_connector_tunnel = Connect("dcris.txt")
-cols = expertise(the_connector_tunnel,supp_key= 112) 
-print("----------------------------------------supp")
+    print(col)
+
+    
+
+tunnel = Connect("dcris.txt")
+cols = expertise(tunnel,supp_key= 112) 
+
+
+print("----------------------------------------supp")
+
+
+
 for col in cols:
-    print(col)
-
-
-
-the_connector_tunnel = Connect("dcris.txt")
-cols = expertise(the_connector_tunnel,keyword = 9) 
-print("----------------------------------------Key")
+    print(col)
+
+
+
+
+
+
+
+tunnel = Connect("dcris.txt")
+cols = expertise(tunnel,keyword = 9) 
+print("----------------------------------------Key")
+
+
+
 for col in cols:
-    print(col)
- 
-
-the_connector_tunnel = Connect("dcris.txt")
-cols = expertise(the_connector_tunnel,supp_key= 112, keyword= 9 ) 
-print("----------------------------------------supp & KEY")
-for col in cols:
-    print(col)
+    print(col)
+
+ 
+
+
+
+
+
+tunnel = Connect("dcris.txt")
+
+cols = expertise(tunnel,supp_key= 112, keyword= 9 ) 
+print("----------------------------------------supp & KEY")
+
+for col in cols:
+
+    print(col)
+
     
